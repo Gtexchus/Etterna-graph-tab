a3477cc048a080019763dea0bbe9408452811d7a
Gtexchus <gtexchus@gmail.com>
2026-08-30 15:18:42 +0100

    scattergraph labels start on a multiple of the scale when using axisLabelScale
    apparrently this was already done for linegraph???? when did I do that????

--- graphs/templates/scatterGraph.lua
+++ graphs/templates/scatterGraph.lua
@@ -246,7 +246,8 @@ local yAxisLabelScale = 1
 --you can either pick labelsScale or labelsCount, not both
 if Var("XaxisLabelScale") then
     xAxisLabelScale = Var("XaxisLabelScale") or 1 --the scale of the x axis labels
-    maxXvalue = ((notShit.floor(maxXvalue  / xAxisLabelScale) + 1) * xAxisLabelScale) + (minXvalue%xAxisLabelScale)--round maxXvalue up to the next x axis label, so the graph will have a label at the right
+    minXvalue = notShit.floor(minXvalue / xAxisLabelScale) * xAxisLabelScale --round minXvalue down to the nearest x axis label, so the labels start on a multiple of the scale
+    maxXvalue = ((notShit.floor(maxXvalue  / xAxisLabelScale) + 1) * xAxisLabelScale)--round maxXvalue up to the next x axis label, so the graph will have a label at the right
     --this is only needed if a scale is entered instead of a count
     xAxisLabelsCount = ((maxXvalue - minXvalue) / xAxisLabelScale) + 1
 else
@@ -256,7 +257,8 @@ end
 
 if Var("YaxisLabelScale") then
     yAxisLabelScale = Var("YaxisLabelScale") or 1 --the scale of y axis labels
-    maxYvalue = ((notShit.floor(maxYvalue / yAxisLabelScale) + 1) * yAxisLabelScale)  + (minYvalue%yAxisLabelScale)--round maxYvalue up to the nearest y axis label, so the graph will have a label at the top
+    minYvalue = notShit.floor(minYvalue / yAxisLabelScale) * yAxisLabelScale --round minYvalue down to the nearest y axis label, so the labels start on a multiple of the scale
+    maxYvalue = ((notShit.floor(maxYvalue / yAxisLabelScale) + 1) * yAxisLabelScale)--round maxYvalue up to the nearest y axis label, so the graph will have a label at the top
     yAxisLabelsCount = ((maxYvalue - minYvalue) / yAxisLabelScale) + 1
 else
     yAxisLabelsCount = Var("YaxisLabelCount") or 1 --how many y axis labels there are
