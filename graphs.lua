aed968fcd5fb412c80bfe10843f3065a0ed22eca
Gtexchus <gtexchus@gmail.com>
2026-09-01 20:11:39 +0100

    basic chart length over time graph

--- graphs.lua
+++ graphs.lua
@@ -61,6 +61,7 @@ local buttons = {
         {graphActorName = "AccuracyOverTimeGraphContainer", graphFileName = "accuracyOverTime", graphButtonText = "Accuracy Over Time"},
         {graphActorName = "MAoverMSDGraphContainer", graphFileName = "MAoverMSD", graphButtonText = "MA over MSD"},
         {graphActorName = "MAoverTimeGraphContainer", graphFileName = "MAoverTime", graphButtonText = "MA over Time"},
+        {graphActorName = "ChartLengthOverTimeGraphContainer", graphFileName = "chartLengthOverTime", graphButtonText = "Chart Length Over Time"},
     },
 
     --line graphs
