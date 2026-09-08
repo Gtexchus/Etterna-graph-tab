33b6159020c19e71ef6121fcbca324caedb55907
Gtexchus <gtexchus@gmail.com>
2026-09-08 17:22:44 +0100

    implemented loadingHandler into meanOverAccuracy

--- graphs/meanOverAccuracy.lua
+++ graphs/meanOverAccuracy.lua
@@ -1,6 +1,8 @@
 local ratios = {
     Width = 780 / 1920, -- width of the box taken from the loading file default.lua
     Height = 612 / 1080,
+    GraphWidth = 680 / 1920,
+    GraphHeight = 412 / 1080,
     SkillsetButtonsX = 590 / 1920,
     SkillsetButtonsY = -80 / 1080,
     SkillsetButtonsHorizontalSpacing = 80 / 1920,
@@ -11,6 +13,8 @@ local ratios = {
 local actuals = {
     Width = ratios.Width * SCREEN_WIDTH,
     Height = ratios.Height * SCREEN_HEIGHT,
+    GraphWidth = ratios.GraphWidth * SCREEN_WIDTH,
+    GraphHeight = ratios.GraphHeight * SCREEN_HEIGHT,
     SkillsetButtonsX = ratios.SkillsetButtonsX * SCREEN_WIDTH,
     SkillsetButtonsY = ratios.SkillsetButtonsY * SCREEN_HEIGHT,
     SkillsetButtonsHorizontalSpacing = ratios.SkillsetButtonsHorizontalSpacing * SCREEN_WIDTH,
@@ -43,90 +47,108 @@ local yAxisLabelTextMaxWidth = ((45 / 1920) * SCREEN_WIDTH) / yAxisLabelTextSize
 
 SCOREMAN:SortRecentScoresForGame()
 
-local function setValues(values)
+local function initialiseValues(values)
     for i = 1, #values do
         table.remove(values, 1)
     end
+end
 
-    for i = 1, SCOREMAN:GetTotalNumberOfScores() do --for every saved score
-        local score = SCOREMAN:GetRecentScoreForGame(i)
-        if score ~= nil then
-            local wife = score:GetWifeScore()
-            if wife >= (minWife) and wife <= (maxWife) and grade ~= "Failed" and grade ~= "Grade_Failed" then
-                local replay = score:GetReplay()
-                replay:LoadAllData()
-                local ov = replay:GetOffsetVector()
-                local mean = wifeMean(ov)
-                
+local function setValues(values, i)
+    local score = SCOREMAN:GetRecentScoreForGame(i)
+    if score ~= nil then
+        local wife = score:GetWifeScore()
+        if wife >= (minWife) and wife <= (maxWife) and grade ~= "Failed" and grade ~= "Grade_Failed" then
+            local replay = score:GetReplay()
+            replay:LoadAllData()
+            local ov = replay:GetOffsetVector()
+            local mean = wifeMean(ov)
+            
 
-                local index = #values + 1
-                values[index] = {}
-                values[index][1] = wife
-                values[index][2] = mean
-                minFoundWife = math.min(minFoundWife, wife)
-                maxFoundWife = math.max(maxFoundWife, wife)
-            end
+            local index = #values + 1
+            values[index] = {}
+            values[index][1] = wife
+            values[index][2] = mean
+            minFoundWife = math.min(minFoundWife, wife)
+            maxFoundWife = math.max(maxFoundWife, wife)
         end
     end
 end
 
 local values = {}
-setValues(values)
-local XaxisLabelCount = (cgf.GetGradeNum(minFoundWife, useMidGrades) - cgf.GetGradeNum(maxFoundWife, useMidGrades)) + 2
 
 
 local t = Def.ActorFrame{
     Name = "MeanOverAccuracyGraphContainer",
+
+    FinishedLoadingCommand = function(self)
+
+        local XaxisLabelCount = (cgf.GetGradeNum(minFoundWife, useMidGrades) - cgf.GetGradeNum(maxFoundWife, useMidGrades)) + 2
+        
+        local graph = LoadActorWithParams("templates/scatterGraph.lua", {
+            Values = values,
+            Xfunc = function(params)
+                return cgf.CoordFuncAcc(params, useMidGrades)
+            end,
+
+            XvalueFunc = function(params)
+                return cgf.ValueFuncAcc(params, useMidGrades)
+            end,
+
+            Yfunc = function(params)
+                return cgf.CoordFuncAsinh(params, scaleDivisor)
+            end,
+
+            YvalueFunc = function(params)
+                return cgf.ValueFuncAsinh(params, scaleDivisor)
+            end,
+            XvalueToStringFunc = cgf.ValueToStringFuncAcc,
+
+            YvalueToStringFunc = function(params)
+                return string.format("%5.2f", params.value)
+            end,
+            MinXvalueFunc = function(params)
+                return cgf.MinValueFuncAcc(params, useMidGrades)
+            end,
+
+            MaxXvalueFunc = function(params)
+                return cgf.MaxValueFuncAcc(params, useMidGrades)
+            end,
+
+            ColorFunc = function(params) return colorByGrade(GetGradeFromPercent(params.xValue)) end,
+
+            XaxisLabelColorFunc = function(params)
+                return cgf.AxisLabelColorFuncAcc(params, xAxisLabelInnerLineAlpha)
+            end,
+
+            YaxisLabelColorFunc = function(params)
+                return{text = color("#ffffff"), outerLine = color("#ffffff"), innerLine = yAxisLabelInnerLineColor}
+            end,
+            GraphWidth = actuals.GraphWidth,
+            GraphHeight = actuals.GraphHeight,
+            PlotAlpha = plotAlpha,
+            XaxisLabelCount = XaxisLabelCount,
+            YaxisLabelCount = YaxisLabelCount,
+            YoriginCentered = true,
+            Xunits = "Accuracy",
+            Yunits = "Mean",
+            YaxisLabelOffset = actuals.YaxisLabelOffset,
+            YaxisLabelTextSize = yAxisLabelTextSize,
+            YaxisLabelTextMaxWidth = yAxisLabelTextMaxWidth
+        })
+
+        self:AddChild(graph)
+    end
 }
 
-t[#t + 1] = LoadActorWithParams("templates/scatterGraph.lua", {
-    Values = values,
-    Xfunc = function(params)
-        return cgf.CoordFuncAcc(params, useMidGrades)
-    end,
 
-    XvalueFunc = function(params)
-        return cgf.ValueFuncAcc(params, useMidGrades)
-    end,
-
-    Yfunc = function(params)
-        return cgf.CoordFuncAsinh(params, scaleDivisor)
-    end,
-
-    YvalueFunc = function(params)
-        return cgf.ValueFuncAsinh(params, scaleDivisor)
-    end,
-    XvalueToStringFunc = cgf.ValueToStringFuncAcc,
-
-    YvalueToStringFunc = function(params)
-        return string.format("%5.2f", params.value)
-    end,
-    MinXvalueFunc = function(params)
-        return cgf.MinValueFuncAcc(params, useMidGrades)
-    end,
-
-    MaxXvalueFunc = function(params)
-        return cgf.MaxValueFuncAcc(params, useMidGrades)
-    end,
-
-    ColorFunc = function(params) return colorByGrade(GetGradeFromPercent(params.xValue)) end,
-
-    XaxisLabelColorFunc = function(params)
-        return cgf.AxisLabelColorFuncAcc(params, xAxisLabelInnerLineAlpha)
-    end,
-
-    YaxisLabelColorFunc = function(params)
-        return{text = color("#ffffff"), outerLine = color("#ffffff"), innerLine = yAxisLabelInnerLineColor}
-    end,
-    PlotAlpha = plotAlpha,
-    XaxisLabelCount = XaxisLabelCount,
-    YaxisLabelCount = YaxisLabelCount,
-    YoriginCentered = true,
-    Xunits = "Accuracy",
-    Yunits = "Mean",
-    YaxisLabelOffset = actuals.YaxisLabelOffset,
-    YaxisLabelTextSize = yAxisLabelTextSize,
-    YaxisLabelTextMaxWidth = yAxisLabelTextMaxWidth
+t[#t + 1] = LoadActorWithParams("commonActors/loadingHandler.lua", {
+    Values = values, 
+    InitialiseValues = initialiseValues, 
+    SetValues = setValues,
+    NumOfValuesToLoad = SCOREMAN:GetTotalNumberOfScores(),
+    Increments = 10,
+    X = actuals.GraphWidth / 2,
+    Y = actuals.GraphHeight / 2
 })
 
 return t
